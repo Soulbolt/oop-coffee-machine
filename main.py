ea9ff51b51d3eb4a41f5c8cea55b4e9a241eb0c2
Soulbolt <soulbolt@gmail.com>
2023-12-12 21:41:02 -0700

    refactored some code.

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ is_machine_on = True
 
 # Keep looping as long as machine is on is True
 while is_machine_on:
+    options = menu.get_items()
     # Ask user to make a choice from given selection
-    choice = input(f"What would you like? {menu.get_items()}\n")
+    choice = input(f"What would you like? {options}\n")
     # Check if user's choice matches proper selection to make a coffee or produce reports or turn off machine
     if choice == "report":
         coffee_maker.report()
@@ -20,22 +21,16 @@ while is_machine_on:
     elif choice == "off":
         is_machine_on = False
         # Check if choice matches the menu item list
-    elif choice in menu.get_items():
-        coffee_available = menu.find_drink(choice)
-        # Check if there are sufficient ingredients to make the coffee and return True
-        if coffee_maker.is_resource_sufficient(coffee_available):
-            # If sufficient ingredients request payment and check if coins inserted >= than the cost
-            if money_machine.make_payment(coffee_available.cost):
-                # If coins inserted cover cost, then make coffee
+    else:
+        if choice in options:
+            coffee_available = menu.find_drink(choice)
+            # Check if there are sufficient ingredients to make the coffee and return True
+            # and check if coins inserted >= than the cost
+            if coffee_maker.is_resource_sufficient(coffee_available) and money_machine.make_payment(coffee_available.cost):
                 coffee_maker.make_coffee(coffee_available)
             else:
-                # If coins do not cover cost return message to user
-                money_machine.make_payment(coffee_available.cost)
+                # If not enough resource available, return message to user
+                coffee_maker.is_resource_sufficient(coffee_available)
         else:
-            # If not enough resource available, return message to user
-            coffee_maker.is_resource_sufficient(coffee_available)
-    else:
-        # IF users choice does not match any of the expected options. return message
-        menu.find_drink(choice)
-
-
+            # IF users choice does not match any of the expected options. return message
+            menu.find_drink(choice)
